dce8862bdd9ae8a2b547aec4c969bb112275d7fc
Rurutia1027 <rurutia1027@gmail.com>
2026-03-20 16:06:57 +0800

    remove local reliable-message-service it is old version impl, not completed impl the reliable message service

--- reliable-message-service/message-service/src/main/java/org/tus/tx/rms/domain/twophase/RmsMessageResultRepository.java
+++
@@ -1,6 +0,0 @@
-package org.tus.tx.rms.domain.twophase;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RmsMessageResultRepository extends JpaRepository<RmsMessageResultEntity, Long> {
-}
